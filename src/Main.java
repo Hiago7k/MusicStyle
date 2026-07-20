7f4faec3976dd216bf77d1711018f738d3c6efc0
Hiago 7k <hiago7k@gmail.com>
2026-07-20 17:22:35 -0300

    primeira musica

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 class Main{
     static void main() {
         System.out.println("Musics & Podcast for public and free");
+
     Musicas musica1 = new Musicas();
     musica1.setTitulo("Impossible");
     musica1.setAutor("James Arthur");
-    //Podcasts falaDevs = new Podcasts();
+    musica1.setClassificacao("Loves Songs");
+    musica1.setCurtidas(50000);
+    musica1.setDuracao(500);
+    musica1.setTotalDeReproducoes(2300000);
+
+        System.out.println("\nExibindo informações da musica " + musica1.getTitulo());
+        System.out.println("Titulo " + musica1.getTitulo());
+        System.out.println("Autor " + musica1.getAutor());
+        System.out.println("Classificação " + musica1.getClassificacao());
+        System.out.println("Número de curtidas " + musica1.getCurtidas());
+        System.out.println("Duração em segundos " + musica1.getDuracao());
+        System.out.println("Visualizações " + musica1.getTotalDeReproducoes());
+
+
+
+
     }
 }
