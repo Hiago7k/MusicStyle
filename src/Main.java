fa07d0f9aaf762f421e4adf66efa3de088dd13b8
Hiago 7k <hiago7k@gmail.com>
2026-07-20 17:18:26 -0300

    propriedade de musica

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 class Main{
     static void main() {
         System.out.println("Musics & Podcast for public and free");
-    Musicas impossible = new Musicas();
-    Podcasts falaDevs = new Podcasts();
+    Musicas musica1 = new Musicas();
+    musica1.setTitulo("Impossible");
+    musica1.setAutor("James Arthur");
+    //Podcasts falaDevs = new Podcasts();
     }
 }
