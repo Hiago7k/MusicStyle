9624acd39b87238f4bc5eacecd5987bb6c244cef
Hiago 7k <hiago7k@gmail.com>
2026-07-20 17:16:01 -0300

    iniciando as sub classes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 class Main{
     static void main() {
         System.out.println("Musics & Podcast for public and free");
-
-        
+    Musicas impossible = new Musicas();
+    Podcasts falaDevs = new Podcasts();
     }
 }
