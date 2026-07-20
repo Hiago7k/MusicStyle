bf42625e61739146decfd76955f9fd45b3e7718e
Hiago 7k <hiago7k@gmail.com>
2026-07-20 17:43:15 -0300

    utilizando this

--- src/Audio.java
+++ src/Audio.java
@@ -1,17 +1,18 @@
 public class Audio {
+
     private String titulo;
     private int duracao;
     private int totalDeReproducoes;
     private int curtidas;
     private String classificacao;
 
-    public void curtirAudio(Audio audio){
-        curtidas = curtidas + 1;
+    public void curtirAudio(){
+        this.curtidas = curtidas + 1;
         System.out.println("Você curtiu esse som");
     }
 
-    public void reproduzirAudio(Audio audio){
-        totalDeReproducoes = totalDeReproducoes + 1;
+    public void reproduzirAudio(){
+        this.totalDeReproducoes = totalDeReproducoes + 1;
         System.out.println("Reproduzindo o audio ");
     };
 
